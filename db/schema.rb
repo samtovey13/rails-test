cf53759bd64de0aacd74d4ae11f65198807e5354
Sam Tovey <samtovey13@gmail.com>
2021-10-04 22:29:55 +0100

    generated scaffolding for Feedback and added minimum length validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_190551) do
+ActiveRecord::Schema.define(version: 2021_10_04_212328) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer "grade"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
